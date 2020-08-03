ea74cb0d6c87a1203ad7246fbb6a8b956a6b60ca
andy8052 <andychorlian@gmail.com>
2020-08-03 11:56:50 -0400

    add mana, knc and zrx

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Chief from './Chief.js';
 import FlipETHA from './FlipETHA.js';
 import FlipBATA from './FlipBATA.js';
 import FlipWBTCA from './FlipWBTCA';
+import FlipKNCA from './FlipKNCA';
+import FlipZRXA from './FlipZRXA';
+import FlipMANAA from './FlipMANAA';
 import Flop from './Flop.js';
 
 function App() {
@@ -17,11 +20,17 @@ function App() {
           mkr.fyi
         </h2>
         <div className="settings">
-          <h4 onClick={() => setPage("FLIPETHA")}>flip eth-a</h4>
+          <h4 onClick={() => setPage("FLIPETHA")}>eth-a</h4>
           <h4>&nbsp;|&nbsp;</h4>
-          <h4 onClick={() => setPage("FLIPBATA")}>flip bat-a</h4>
+          <h4 onClick={() => setPage("FLIPBATA")}>bat-a</h4>
           <h4>&nbsp;|&nbsp;</h4>
-          <h4 onClick={() => setPage("FLIPWBTCA")}>flip wbtc-a</h4>
+          <h4 onClick={() => setPage("FLIPWBTCA")}>wbtc-a</h4>
+          <h4>&nbsp;|&nbsp;</h4>
+          <h4 onClick={() => setPage("FLIPKNCA")}>knc-a</h4>
+          <h4>&nbsp;|&nbsp;</h4>
+          <h4 onClick={() => setPage("FLIPZRXA")}>zrx-a</h4>
+          <h4>&nbsp;|&nbsp;</h4>
+          <h4 onClick={() => setPage("FLIPMANAA")}>mana-a</h4>
           <h4>&nbsp;|&nbsp;</h4>
           <h4 onClick={() => setPage("FLOP")}>flop</h4>
           <h4>&nbsp;|&nbsp;</h4>
@@ -37,6 +46,12 @@ function App() {
               return <FlipBATA/>;
             case 'FLIPWBTCA':
               return <FlipWBTCA/>;
+            case 'FLIPKNCA':
+              return <FlipKNCA/>;
+            case 'FLIPZRXA':
+              return <FlipZRXA/>;
+            case 'FLIPMANAA':
+              return <FlipMANAA/>;
             case 'FLOP':
               return <Flop/>;
             default:
